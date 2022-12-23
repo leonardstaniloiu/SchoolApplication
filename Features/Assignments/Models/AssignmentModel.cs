1a67118b66a59bdd272a55507d4643b4c4d0ca88
Leonard <49596089+leonardstaniloiu@users.noreply.github.com>
2022-12-23 16:42:53 +0200

    update

---
+++ Features/Assignments/Models/AssignmentModel.cs
@@ -0,0 +1,12 @@
+﻿using School.Base;
+
+namespace School.Features.Assignments.Models;
+
+public class AssignmentModel : Model
+{
+    public string Id { get; set; }
+    public string Subject { get; set; }
+    public string Description { get; set; }
+    public DateTime Deadline { get; set; }
+    
+}
---
+++ Features/Assignments/Views/AssignmentsResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace School.Features.Assignments.Views;
+
+public class AssignmentsResponse
+{
+    public string Id { get; set; }
+    public string Subject { get; set; }
+    public string Description { get; set; }
+    public DateTime Deadline { get; set; }
+    public bool Success { get; set; }
+    public string Mesaj { get; set; }
+}
